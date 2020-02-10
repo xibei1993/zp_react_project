e68e1dd9ad1fb2d63a25bcb36cf0dc35abbcc9f8
xibei1993 <24406831@qq.com>
2020-02-10 21:06:59 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+import {Route,Switch} from 'react-router-dom'
 
 export default class App extends Component {
     render() {
         return (
-            <h1>
-                APP
-            </h1>
+           <Switch>
+               <Route path="/login"  component={Login}/>
+               <Route path="/admin"  component={Admin}/>
+
+           </Switch>
         )
     }
 }
